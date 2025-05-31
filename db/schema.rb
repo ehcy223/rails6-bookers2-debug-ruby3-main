5059980aa882b2910a62d1d024eff58e963d5273
Eri Higuchi <209428143+ehcy223@users.noreply.github.com>
2025-05-31 15:39:02 +0000

    課題⒊いいね機能途中

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_071114) do
+ActiveRecord::Schema.define(version: 2025_05_31_003344) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 2021_10_21_071114) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_image_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "post_image_id"], name: "index_favorites_on_user_id_and_post_image_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
