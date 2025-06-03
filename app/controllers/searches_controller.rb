7ad4083d23e0869494002902832f4e5f100d0183
Eri Higuchi <209428143+ehcy223@users.noreply.github.com>
2025-06-03 07:21:01 +0000

    課題５終了

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,35 @@
 class SearchesController < ApplicationController
+  def search
+    @model = params[:model]        # "user" または "book"
+    @content = params[:content]    # 検索ワード
+    @method = params[:method]      # "perfect", "forward", "backward", "partial"
+
+    @records = search_for(@model, @content, @method)
+  end
+
+  private
+
+  def search_for(model, content, method)
+    if model == "user"
+      if method == "perfect"
+        User.where(name: content)
+      elsif method == "forward"
+        User.where("name LIKE ?", "#{content}%")
+      elsif method == "backward"
+        User.where("name LIKE ?", "%#{content}")
+      else
+        User.where("name LIKE ?", "%#{content}%")
+      end
+    elsif model == "book"
+      if method == "perfect"
+        Book.where(title: content)
+      elsif method == "forward"
+        Book.where("title LIKE ?", "#{content}%")
+      elsif method == "backward"
+        Book.where("title LIKE ?", "%#{content}")
+      else
+        Book.where("title LIKE ?", "%#{content}%")
+      end
+    end
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,7 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_by_users, through: :favorites, source: :user
   has_many :book_comments, dependent: :destroy
+  has_one_attached :book_image
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
